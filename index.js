3ab9a9e57a5f80e38cceb6508e492686032a0ed9
Omar Razi <startfresh87@gmail.com>
2021-10-10 22:04:42 -0400

    commit tree 18a4f7e699b1b7f6a2001b77d456defdf5ba8a53

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("relax");
+
+const mondayWork = function(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+mondayWork("work from home");
+
+function wrapAdjective(string = "*") {
+    return function(you = "special") {
+        return `You are ${string}${you}${string}!`
+    }
+
+}
